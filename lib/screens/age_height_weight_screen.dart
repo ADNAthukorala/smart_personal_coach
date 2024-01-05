8267c141581cb15282e9820b1ef11d265a9fd0ae
Dumindu Athukorala <117508037+ADNAthukorala@users.noreply.github.com>
2024-01-05 09:06:10 +0530

    Changed

--- lib/screens/age_height_weight_screen.dart
+++ lib/screens/age_height_weight_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:smart_personal_coach/constants.dart';
+import 'package:smart_personal_coach/components.dart';
 
 class AgeHeightWeightScreen extends StatefulWidget {
   const AgeHeightWeightScreen({super.key});
@@ -11,7 +13,28 @@ class _AgeHeightWeightScreenState extends State<AgeHeightWeightScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            //Top texts column of bottom area
+            const Column(
+              children: [
+                Text(
+                  'Sign In',
+                  style: kLargeTextStyle,
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  'Please enter email and password for login',
+                  style: kSmallTextStyle,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
