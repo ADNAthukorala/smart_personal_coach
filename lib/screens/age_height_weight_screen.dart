4f1bd981244898855013f8fa22906d8cd50f35e4
Dumindu Athukorala <117508037+ADNAthukorala@users.noreply.github.com>
2024-01-05 09:43:23 +0530

    Changed

--- lib/screens/age_height_weight_screen.dart
+++ lib/screens/age_height_weight_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:smart_personal_coach/components/constants.dart';
-import 'package:smart_personal_coach/components.dart';
 
 class AgeHeightWeightScreen extends StatefulWidget {
   const AgeHeightWeightScreen({super.key});
@@ -13,7 +12,12 @@ class _AgeHeightWeightScreenState extends State<AgeHeightWeightScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('ADNA'),
+        backgroundColor: kBlueThemeColor,
+      ),
       body: Padding(
+        //Adding padding to screen
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
@@ -21,16 +25,59 @@ class _AgeHeightWeightScreenState extends State<AgeHeightWeightScreen> {
             const Column(
               children: [
                 Text(
-                  'Sign In',
+                  'Let us known you better',
                   style: kLargeTextStyle,
                   textAlign: TextAlign.center,
                 ),
                 Text(
-                  'Please enter email and password for login',
+                  'Let us know you better to help boost your workout results',
                   style: kSmallTextStyle,
                   textAlign: TextAlign.center,
                 ),
               ],
+            ),
+            //User's data getting card
+            Card(
+              color: kBlueThemeColor,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text(
+                          'Age',
+                          style: kLargeTextStyle,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.baseline,
+                          textBaseline: TextBaseline.alphabetic,
+                          children: [
+                            Text(
+                              '22',
+                              style: kLargeTextStyle.copyWith(color: kWhiteThemeColor),
+                            ),
+                            Text(
+                              'years',
+                              style: kSmallTextStyle.copyWith(color: kWhiteThemeColor),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Slider(
+                      value: 100.0,
+                      min: 60.0,
+                      max: 280.0,
+                      onChanged: (value) {
+
+                      },
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
