7cebfe0389dbf11ab2e64a2732985cf7f8783239
Dumindu Athukorala <117508037+ADNAthukorala@users.noreply.github.com>
2024-01-28 11:36:03 +0530

    Changed

--- lib/screens/bottom_navigation_bar.dart
+++ lib/screens/bottom_navigation_bar.dart
@@ -9,7 +9,8 @@ class BottomNavigationBarScreen extends StatefulWidget {
   const BottomNavigationBarScreen({super.key});
 
   @override
-  State<BottomNavigationBarScreen> createState() => _BottomNavigationBarScreenState();
+  State<BottomNavigationBarScreen> createState() =>
+      _BottomNavigationBarScreenState();
 }
 
 class _BottomNavigationBarScreenState extends State<BottomNavigationBarScreen> {
@@ -32,23 +33,44 @@ class _BottomNavigationBarScreenState extends State<BottomNavigationBarScreen> {
             _currentScreenIndex = index;
           });
         },
-        indicatorColor: kBlueThemeColor,
+        indicatorColor: Colors.transparent,
         selectedIndex: _currentScreenIndex,
-        destinations: const [
+        labelBehavior: NavigationDestinationLabelBehavior.alwaysHide,
+        destinations: [
           NavigationDestination(
-            icon: Icon(Icons.home_rounded),
+            icon: Icon(
+              size: 30.0,
+              Icons.home_rounded,
+              color:
+                  _currentScreenIndex == 0 ? kBlueThemeColor : kGreyThemeColor,
+            ),
             label: 'Home',
           ),
           NavigationDestination(
-            icon: Icon(Icons.travel_explore_rounded),
+            icon: Icon(
+              size: 30.0,
+              Icons.travel_explore_rounded,
+              color:
+                  _currentScreenIndex == 1 ? kBlueThemeColor : kGreyThemeColor,
+            ),
             label: 'Global Chat',
           ),
           NavigationDestination(
-            icon: Icon(Icons.bar_chart_rounded),
+            icon: Icon(
+              size: 30.0,
+              Icons.bar_chart_rounded,
+              color:
+                  _currentScreenIndex == 2 ? kBlueThemeColor : kGreyThemeColor,
+            ),
             label: 'Report',
           ),
           NavigationDestination(
-            icon: Icon(Icons.settings_rounded),
+            icon: Icon(
+              size: 30.0,
+              Icons.settings_rounded,
+              color:
+                  _currentScreenIndex == 3 ? kBlueThemeColor : kGreyThemeColor,
+            ),
             label: 'Settings',
           ),
         ],
