af21fb575f981f95e1160f3863f11f8a3ffec7a9
Dumindu Athukorala <117508037+ADNAthukorala@users.noreply.github.com>
2024-02-02 02:54:56 +0530

    Added firebase and Roboto font

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        fontFamily: 'Roboto',
         drawerTheme: const DrawerThemeData(
           backgroundColor: kWhiteThemeColor,
         ),
