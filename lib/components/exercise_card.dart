04bfb67a0a5723ad74abffb0a6a4a8270613f172
Dumindu Athukorala <117508037+ADNAthukorala@users.noreply.github.com>
2024-03-08 10:26:24 +0530

    Changed

--- lib/components/exercise_card.dart
+++ lib/components/exercise_card.dart
@@ -1,8 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:smart_personal_coach/constants.dart';
 
 class ExerciseCard extends StatelessWidget {
