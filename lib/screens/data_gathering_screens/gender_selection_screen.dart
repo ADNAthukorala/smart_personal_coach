a87cd14d2873e78eeb6a03b2169e33bc7008ce33
Dumindu Athukorala <117508037+ADNAthukorala@users.noreply.github.com>
2024-05-10 06:02:12 +0530

    Changed

--- lib/screens/data_gathering_screens/gender_selection_screen.dart
+++ lib/screens/data_gathering_screens/gender_selection_screen.dart
@@ -109,12 +109,6 @@ class _GenderSelectionScreenState extends State<GenderSelectionScreen> {
     );
   }
 
-  /// Send verification email to the user's email
-  Future<void> _sendEmailVerification() async {
-    final user = _auth.currentUser;
-    await user?.sendEmailVerification();
-  }
-
   @override
   void initState() {
     getLoggedIntUser();
@@ -217,18 +211,6 @@ class _GenderSelectionScreenState extends State<GenderSelectionScreen> {
                   onPressed: _userGender == "Not Selected"
                       ? null // Disable the next button
                       : () {
-                          if (loggedInUser.emailVerified) {
-                          } else {
-                            // Send email verification to signed in user' email
-                            _sendEmailVerification();
-                            // Show snack bar with message
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                  content: Text("Sent an email verification."
-                                      " Please check your email inbox and verify the email to proceed!")),
-                            );
-                          }
-
                           // When the button is clicked, navigate to the age, height, weight screen
                           Navigator.push(
                             context,
