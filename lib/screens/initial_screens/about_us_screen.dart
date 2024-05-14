a3e0a7dd3848f578b31bd3233e728f450db0e178
Dumindu Athukorala <117508037+ADNAthukorala@users.noreply.github.com>
2024-05-15 02:36:55 +0530

    Changed

---
+++ lib/screens/initial_screens/about_us_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:smart_personal_coach/constants.dart';
+
+/// About us
+class AboutUsScreen extends StatefulWidget {
+  const AboutUsScreen({super.key});
+
+  @override
+  State<AboutUsScreen> createState() =>
+      _AboutUsScreenState();
+}
+
+class _AboutUsScreenState extends State<AboutUsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      /// Appbar
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          'About Us',
+          style: kAppBarTextStyle,
+        ),
+      ),
+
+      /// Body of the screen
+      body: Padding(
+        // Add padding around the body content
+        padding: const EdgeInsets.all(kPadding16),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView(
+                // Include all components in a list view and set padding around the components to 0
+                padding: const EdgeInsets.only(
+                  top: kPadding16,
+                  bottom: kPadding16,
+                ),
+                primary: false,
+                children: const [
+                  Text('ADNA DEVELOPER'),
+                ],
+              ),
+            ),
+
+            /// Adding space
+            const SizedBox(height: 8.0),
+
+            /// Go-back button
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              style: kNextButtonStyle,
+              child: const Text(
+                'OK',
+                style: kNextButtonTextStyle,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
